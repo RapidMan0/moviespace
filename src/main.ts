4dfaf53506ed446a202c114aa4406de755f7aaf0
RapidMan0 <vceb12323@gmail.com>
2025-09-25 14:16:22 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Moviespace API')
+    .setTitle('Moviespace')
     .setDescription('API documentation for Moviespace')
     .setVersion('1.0')
     .build();
